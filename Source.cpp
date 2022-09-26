02210489e88d19a260e17610d5c1cf583d5f1019
mizzo-pizzo <114093387+mizzo-pizzo@users.noreply.github.com>
2022-09-26 18:39:09 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+
+
+/*	int a, b;
+	cin >> a >> b;
+	if (a > b) {
+		cout << a << endl;
+	}
+	else if (b < a) {
+		cout << b << endl;
+	}
+	cout << a << b << endl; */
+	 
+	//if 8
+	/*int a, b;
+	cin >> a >> b;
+	if (a < b) {
+		cout << a << endl;
+		cout << b << endl;
+	}
+	else if (b > a) {
+		cout << b << endl;
+		cout << a << endl;
+	}
+	else if (a == b) {
+		cout << "equals" << endl;
+	}*/
+
+	//if 9
+	int
+
+}
+
